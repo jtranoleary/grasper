7de3335f7c9659829e340a4ad6c8d80254e726aa
Jasper Tran O'Leary <jtranoleary@google.com>
2025-03-02 00:18:53 +0000

    Add basic semi-implicit Euler for particles
    This commit adds the beginning of physics on the Rust side. We keep a scene
    state and pass the particles to the Typescript side using a function call.

--- simulation/src/lib.rs
+++ simulation/src/lib.rs
@@ -14,9 +14,80 @@
 
 // Entry point for the fluid simulation.
 
+use instant::Instant;
+use js_sys;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn greet() -> String {
-    "Hello world from WASM!".to_string()
+pub struct Particle {
+    x: f32,
+    y: f32,
+    z: f32,
+    vy: f32
+}
+
+#[wasm_bindgen]
+pub struct Simulation {
+    particles: Vec<Particle>,
+    floor_y: f32,
+    gravity: f32,
+    last_update_time: Option<Instant>
+}
+
+#[wasm_bindgen]
+impl Simulation {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Simulation {
+        let num_particles = 10000;
+        let bounding_box_dim = 5.0;
+        let y_offset = 2.0;
+        let mut particles = Vec::with_capacity(num_particles);
+
+        for _ in 0..num_particles {
+            particles.push(Particle {
+                x: (js_sys::Math::random() as f32 - 0.5) * bounding_box_dim,
+                y: (js_sys::Math::random() as f32 - 0.5) * bounding_box_dim +
+                    y_offset,
+                z: (js_sys::Math::random() as f32 - 0.5) * bounding_box_dim,
+                vy: 0.0
+            });
+        }
+
+        Simulation {
+            particles,
+            floor_y: 0.0,
+            gravity: -9.8,
+            last_update_time: Some(instant::Instant::now())
+        }
+    }
+
+    pub fn update(&mut self) {
+        let now = instant::Instant::now();
+        let dt = match self.last_update_time {
+            Some(last_time) => now.duration_since(last_time)
+                                           .as_secs_f32(),
+            None => 1.0 / 60.0
+        };
+        self.last_update_time = Some(now);
+
+        for particle in &mut self.particles {
+            particle.vy += self.gravity * dt;
+            particle.y += particle.vy * dt;
+            if particle.y < self.floor_y {
+                particle.y = self.floor_y;
+                particle.vy = 0.0;
+            }
+        }
+    }
+
+    pub fn get_particle_positions(&self) -> Vec<f32> {
+        let mut positions = Vec::with_capacity(
+            self.particles.len() * 3);
+        for particle in &self.particles {
+            positions.push(particle.x);
+            positions.push(particle.y);
+            positions.push(particle.z);
+        }
+        positions
+    }
 }
